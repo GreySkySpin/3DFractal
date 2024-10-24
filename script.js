bc1ee63657ed342d3f022210d695de08ce8e8b97
GreySkySpin <42599782+GreySkySpin@users.noreply.github.com>
2024-10-24 21:19:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 	const renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	document.getElementById('visualization').appendChild(renderer.domElement);
+	document.getElementById('visualisation').appendChild(renderer.domElement);
 
 	// Create the center node
 	const centerNode = new THREE.Mesh(
